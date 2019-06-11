0bab886766f6ae3f927c9454e49f443a87df3f05
Brian Burnett <hikerspath@users.noreply.github.com>
2019-06-11 00:41:54 -0400

    Revert to lab1 starting point

--- server.tf
+++ server.tf
@@ -1,33 +1,11 @@
 # AWS Credentials
-variable "access_key" {}
-variable "secret_key" {}
-variable "region" {
-  default = "eu-west-1"
-}
-
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+  access_key = ""
+  secret_key = ""
+  region     = ""
 }
 
 # EC2 Instance
-variable "ami" {}
-variable "subnet_id" {}
-variable "identity" {}
-variable "vpc_security_group_id" {}
-
 resource "aws_instance" "web" {
-  ami           = "${var.ami}"
-  instance_type = "t2.micro"
-  
-  subnet_id              = "${var.subnet_id}"
-  vpc_security_group_ids = ["${var.vpc_security_group_id}"]
-  
-  tags {
-    "Identity"    = "${var.identity}"
-    "Name"        = "Student"
-    "Environment" = "Training"
-    "Vpc"         = "tf-101"
-  }
+  # ...
 }
